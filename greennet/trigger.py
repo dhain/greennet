d02d81ab88f6651fea954710d3600d14a4601be8
David Hain <dhain@webskewer.org>
2008-10-26 11:16:37 -0700

    add close() to trigger, handle some errors

--- greennet/trigger.py
+++ greennet/trigger.py
@@ -2,22 +2,47 @@
 
 
 import os
+import errno
 
 from greennet import get_hub
 
 
 class Trigger(object):
+    
+    __slots__ = ('hub', '_gun', '_trigger', '_closed')
+    
     def __init__(self, hub=None):
         self.hub = get_hub() if hub is None else hub
         self._gun, self._trigger = os.pipe()
+        self._closed = False
     
     def wait(self, timeout=None):
+        if self._closed:
+            raise IOError(errno.EBADF, os.strerror(errno.EBADF))
         self.hub.poll(self._gun, read=True, timeout=timeout)
         os.read(self._gun, 1)
     
     def pull(self):
-        while not os.write(self._trigger, 'x'):
-            pass
+        if self._closed:
+            raise IOError(errno.EBADF, os.strerror(errno.EBADF))
+        while True:
+            try:
+                os.write(self._trigger, 'x')
+            except (IOError, OSError), err:
+                if err.args[0] == errno.EINTR:
+                    continue
+                elif err.args[0] == errno.EAGAIN:
+                    return
+                raise
+    
+    def close(self):
+        self._closed = True
+        for fd in self._gun, self._trigger:
+            try:
+                os.close(fd)
+            except IOError:
+                pass
+        del self._gun, self._trigger
 
 
 if __name__ == '__main__':
