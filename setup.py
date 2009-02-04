e7bf50bf7d86329d3a9c2dbaca71a6879b47b848
David Hain <dhain@webskewer.org>
2009-02-03 18:20:18 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(
+    name='greennet',
+    version=version,
+    description=('A greenlet-based task scheduler'),
+    author='David Hain',
+    author_email='dhain@zognot.org',
+    url='http://zognot.org/projects/greennet/',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'py',
+    ],
+)
